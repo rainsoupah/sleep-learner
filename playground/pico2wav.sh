bd27b7d20f641b7f20411fdf253c74667804e13e
luckytoilet <bai.li.2005@gmail.com>
2016-11-12 03:26:49 -0500

    Change to using pico2wave which produces better sounds

---
+++ playground/pico2wav.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pico2wave -l=en-US -w=/tmp/test.wav "$1"
+aplay /tmp/test.wav
+rm /tmp/test.wav
--- playground/test.sh
+++ playground/test.sh
@@ -1,43 +1,45 @@
 #/bin/bash
-# Test using espeak tool to generate computer speech
-espeak "abject"
-espeak "of the most contemptible kind"
+
+# Test using text to speech tools
+# espeak and pico2wave are options
+./pico2wav.sh "abject"
+./pico2wav.sh "of the most contemptible kind"
 sleep 1
-espeak "aberration"
-espeak "a state or condition markedly different from the norm"
+./pico2wav.sh "aberration"
+./pico2wav.sh "a state or condition markedly different from the norm"
 sleep 1
-espeak "abjure"
-espeak "formally reject or disavow a formerly held belief"
+./pico2wav.sh "abjure"
+./pico2wav.sh "formally reject or disavow a formerly held belief"
 sleep 1
-espeak "abnegation"
-espeak "the denial and rejection of a doctrine or belief"
+./pico2wav.sh "abnegation"
+./pico2wav.sh "the denial and rejection of a doctrine or belief"
 sleep 1
-espeak "abrogate"
-espeak "revoke formally"
+./pico2wav.sh "abrogate"
+./pico2wav.sh "revoke formally"
 sleep 1
-espeak "abscond"
-espeak "run away, often taking something or somebody along"
+./pico2wav.sh "abscond"
+./pico2wav.sh "run away, often taking something or somebody along"
 sleep 1
-espeak "abstruse"
-espeak "difficult to penetrate"
+./pico2wav.sh "abstruse"
+./pico2wav.sh "difficult to penetrate"
 sleep 1
-espeak "accede"
-espeak "yield to another's wish or opinion"
+./pico2wav.sh "accede"
+./pico2wav.sh "yield to another's wish or opinion"
 sleep 1
-espeak "accost"
-espeak "speak to someone"
+./pico2wav.sh "accost"
+./pico2wav.sh "speak to someone"
 sleep 1
-espeak "accretion"
-espeak "an increase by natural growth or addition"
+./pico2wav.sh "accretion"
+./pico2wav.sh "an increase by natural growth or addition"
 sleep 1
-espeak "acumen"
-espeak "shrewdness shown by keen insight"
+./pico2wav.sh "acumen"
+./pico2wav.sh "shrewdness shown by keen insight"
 sleep 1
-espeak "adamant"
-espeak "impervious to pleas, persuasion, requests, reason"
+./pico2wav.sh "adamant"
+./pico2wav.sh "impervious to pleas, persuasion, requests, reason"
 sleep 1
-espeak "admonish"
-espeak "scold or reprimand; take to task"
+./pico2wav.sh "admonish"
+./pico2wav.sh "scold or reprimand; take to task"
 sleep 1
-espeak "adumbrate"
-espeak "describe roughly or give the main points or summary of"
+./pico2wav.sh "adumbrate"
+./pico2wav.sh "describe roughly or give the main points or summary of"
