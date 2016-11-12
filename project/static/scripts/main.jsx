dbd1e128f1226275fdce839b772db75c63c1c9cb
yutong <yutong.wang.94@gmail.com>
2016-11-12 18:41:02 -0500

    add counter

--- project/static/scripts/main.jsx
+++ project/static/scripts/main.jsx
@@ -1,17 +1,37 @@
 var SleepTest = React.createClass({
+  getInitialState() {
+    return { 
+      currentCard : 1,
+      totalCards: 100
+    };
+  },
+
+  ClickYES(e) {
+    console.log("CLicked Yes");
+    this.setState({currentCard: this.state.currentCard+1});
+
+  },
+
+  ClickNO(e) {
+    console.log("Clicked No");
+    this.setState({currentCard: this.state.currentCard+1});
+  },
+
   render: function() {
     return (
       <div>
-        <h2>Hello Sleep Learner</h2>
-        <audio controls>
-          <source src="/sound.wav" type="audio/wav" />
-        </audio>
+        <h2>{this.state.currentCard}/{this.state.totalCards}</h2>
+        <div>
+        <p>{this.props.word}</p>
+         <button className="btn btn-default" onClick={this.ClickYES}>YES</button>
+         <button className="btn btn-default" onClick={this.ClickNO}>NO</button>
+        </div>
       </div>
     );
   }
 });
 
 ReactDOM.render(
-  React.createElement(SleepTest, null),
+  <SleepTest word="Facetious"/>,
   document.getElementById('root')
 );
