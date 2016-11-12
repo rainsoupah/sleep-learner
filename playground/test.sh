f2fcdbf354b267ef3316e2a96c3be00c149bbab0
luckytoilet <bai.li.2005@gmail.com>
2016-11-12 03:06:42 -0500

    Prototype using espeak command

---
+++ playground/test.sh
@@ -0,0 +1,43 @@
+#/bin/bash
+# Test using espeak tool to generate computer speech
+espeak "abject"
+espeak "of the most contemptible kind"
+sleep 1
+espeak "aberration"
+espeak "a state or condition markedly different from the norm"
+sleep 1
+espeak "abjure"
+espeak "formally reject or disavow a formerly held belief"
+sleep 1
+espeak "abnegation"
+espeak "the denial and rejection of a doctrine or belief"
+sleep 1
+espeak "abrogate"
+espeak "revoke formally"
+sleep 1
+espeak "abscond"
+espeak "run away, often taking something or somebody along"
+sleep 1
+espeak "abstruse"
+espeak "difficult to penetrate"
+sleep 1
+espeak "accede"
+espeak "yield to another's wish or opinion"
+sleep 1
+espeak "accost"
+espeak "speak to someone"
+sleep 1
+espeak "accretion"
+espeak "an increase by natural growth or addition"
+sleep 1
+espeak "acumen"
+espeak "shrewdness shown by keen insight"
+sleep 1
+espeak "adamant"
+espeak "impervious to pleas, persuasion, requests, reason"
+sleep 1
+espeak "admonish"
+espeak "scold or reprimand; take to task"
+sleep 1
+espeak "adumbrate"
+espeak "describe roughly or give the main points or summary of"
