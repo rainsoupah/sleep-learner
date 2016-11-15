4e1294d57e6845367bd0ccd72d63330ef6cd877d
yutong <yutong.wang.94@gmail.com>
2016-11-14 19:56:25 -0500

    more words now

--- project/controllers/player.py
+++ project/controllers/player.py
@@ -2,9 +2,15 @@ from flask import render_template
 from flask import Blueprint
 from flask import send_file
 from flask import request
+from flask import jsonify
 
 from util.text_to_speech import TextToSpeech
 from util.text_to_speech import TMP_DIR
+
+import csv
+import json
+import os
+import urllib2, urllib
 
 player_app = Blueprint('player', __name__)
 
@@ -18,6 +24,12 @@ def request_tts():
   text = request.args.get('text')
   url = TextToSpeech.generate_wave(text)
   return '/player/wav/' + url;
+
+@player_app.route('/player/words')
+def request_word():
+	# test data wordlist
+	wordlist=[{'word': "a", 'defin': "a is a"},{'word': "b", 'defin': "b is b"}, {'word': "c", 'defin': "c is c"}, {'word': "d", 'defin': "d is d"}, {'word': "e", 'defin': "e is e"},{'word': "f", 'defin': "f is f"},{'word': "g", 'defin': "g is g"},{'word': "h", 'defin': "h is h"},{'word': "i", 'defin': "i is i"},{'word': "j", 'defin': "j is j"}]
+	return jsonify(results=wordlist)
 
 @player_app.route('/player/wav/<path:url>')
 def retrieve_wav(url):
--- project/static/scripts/main.jsx
+++ project/static/scripts/main.jsx
@@ -1,18 +1,14 @@
-function distance( x1, y1, x2, y2 ) {
-  var dx = x1-x2;
-  var dy = y1-y2;
-  return Math.sqrt( dx*dx + dy*dy );
-}
-
 var SleepTest = React.createClass({
   getInitialState() {
     return { 
       currentCard : 1,
-      totalCards: 10
+      totalCards: 10,
+      wordSet: {}
     };
   },
 
   yes(e) {
+    document.querySelector('.card').classList.toggle('flipped');
     if (this.state.currentCard >= this.state.totalCards) {
       console.log("Go to next page");
       // reset currentCard, go to different react router
@@ -25,26 +21,37 @@ var SleepTest = React.createClass({
   },
 
   no(e) {
-    this.setState({currentCard: this.state.currentCard+1});
+    document.querySelector('.card').classList.toggle('flipped');
+    if (this.state.currentCard >= this.state.totalCards) {
+      console.log("Go to next page");
+      // reset currentCard, go to different react router
+      this.setState({currentCard:1});
+    } else {
+      this.setState({currentCard: this.state.currentCard+1});
+      // record in database
+    }
   },
 
-  flip(e) {
-    document.querySelector('.card').classList.toggle('flipped');
+  handleData:function(data) {
+    word = data.results[this.state.currentCard-1];
+    this.setState({wordSet:word});
   },
 
   render: function() {
     var yes = "I know";
     var no = "I don't know";
-    var defin = "treating serious issues with deliberately inappropriate humor; flippant.late 16th century (in the general sense ‘witty, amusing’): from French facétieux, from facétie, from Latin facetia ‘jest,’ from facetus ‘witty.’"
+
+    // ajax request word call
+    $.getJSON('/player/words', this.handleData);
 
     return (
-      <div className="card" onClick={this.flip}>
+      <div className="card">
         <div className="front">
           <h2>Word {this.state.currentCard}/{this.state.totalCards}</h2>
-          <div className="subheader">{this.props.word}</div>
-          <p>{defin}</p>
-          <button className="Green" onClick={this.yes}>{yes}</button>
-          <button className="Red" onClick={this.no}>{no}</button>
+          <div className="subheader">{this.state.wordSet.word}</div>
+          <p>{this.state.wordSet.defin}</p>
+          <button className="hvr-float-shadow" onClick={this.yes}>{yes}</button>
+          <button style={{float: 'right'}} className="hvr-float-shadow" onClick={this.no}>{no}</button>
         </div>
         
       </div>   
@@ -53,6 +60,18 @@ var SleepTest = React.createClass({
 });
 
 ReactDOM.render(
-  <SleepTest word="Facetious"/>,
+  <SleepTest/>,
   document.getElementById('root')
 );
+
+    /*// Alternative: infinite loop over data do not use
+    $.ajax({
+      url: "/player/words",
+      type: 'GET',
+      success: function(data) {
+        console.log("got word");
+        //word = data.results[0];
+        word = data.results[this.state.currentCard-1];
+        this.setState({wordSet:word});
+      }
+    });*/
