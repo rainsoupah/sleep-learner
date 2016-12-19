0fd563e2599aeb0f525e446c27f18d189a1cd19d
luckytoilet <bai.li.2005@gmail.com>
2016-12-19 14:17:30 -0500

    Clean up quiz JS logic

--- project/static/src/main.jsx
+++ project/static/src/main.jsx
@@ -7,7 +7,6 @@ class SleepTest extends React.Component {
       currentCard : 0,
       totalCards: 0,
       cardList: [],
-      display: "none",
     }
   }
 
@@ -28,7 +27,11 @@ class SleepTest extends React.Component {
 
   }
 
-  yes(e) {
+  /**
+   * Called when user clicks yes or no button, make POST request to the
+   * server and show the next card
+   */
+  processUserResponse(responseYes) {
     if (this.state.currentCard == this.state.totalCards-2) {
       this.setState({
         btn_know: "proceed to sleep learn",
@@ -36,74 +39,35 @@ class SleepTest extends React.Component {
       });
     }
     if (this.state.currentCard >= this.state.totalCards - 1) {
-      console.log("Go to next page");
-      // reset currentCard, redirect to play page
-      this.setState({currentCard:0});
-      this.redirect();
+      // Redirect to play page
+      location.href='player';
     } else {
       this.setState({currentCard: this.state.currentCard+1});
     }
 
     // record in database
     let currentCard = this.state.cardList[this.state.currentCard];
-    var data = {
-      "know": 1,
-      "word": currentCard.word
-    }
+
     // Submit form via jQuery/AJAX
     $.ajax({
       type: 'POST',
-      data: JSON.stringify(data),
+      data: {
+        "know": responseYes,
+        "word": currentCard.word,
+      },
       url: '/quiz/reply',
       contentType:"application/json"
-    })
-    .done(function(data) {
-      console.log('success')
-    })
-    .fail(function(jqXhr) {
-      console.log('failed to register');
     });
   }
 
-  no(e) {
-    if (this.state.currentCard == this.state.totalCards-2) {
-      this.setState({
-        btn_know: "proceed to sleep learn",
-        btn_nknow: "proceed to sleep learn",
-      });
-    }
-    if (this.state.currentCard >= this.state.totalCards - 1) {
-      console.log("Go to next page");
-      // reset currentCard, go to player page
-      this.setState({currentCard:0});
-      this.redirect();
-    } else {
-      this.setState({currentCard: this.state.currentCard+1});
-    }
-    // record in database
-    let currentCard = this.state.cardList[this.state.currentCard];
-    var data = {
-      "know": 0,
-      "word": currentCard.word
-    }
-    // Submit form via jQuery/AJAX
-    $.ajax({
-      type: 'POST',
-      data: JSON.stringify(data),
-      url: '/quiz/reply',
-      contentType:"application/json"
-    })
-    .done(function(data) {
-      console.log('success')
-    })
-    .fail(function(jqXhr) {
-      console.log('failed to register');
-    });
+  onClickYes(e) {
+    this.processUserResponse(1);
   }
 
-  redirect(e) {
-    location.href='player';
+  onClickNo(e) {
+    this.processUserResponse(0);
   }
+
 
   render() {
     if(this.state.cardList.length == 0){
@@ -127,20 +91,16 @@ class SleepTest extends React.Component {
               {currentCard.defin}
             </p>
 
-            <button className="hvr-float-shadow" onClick={this.yes.bind(this)}>
+            <button className="hvr-float-shadow btn btn-yes" onClick={this.onClickYes.bind(this)}>
               {this.state.btn_know}
             </button>
 
-            <button style={{float: 'right'}} className="hvr-float-shadow" onClick={this.no.bind(this)}>
+            <button className="hvr-float-shadow btn btn-no" onClick={this.onClickNo.bind(this)}>
               {this.state.btn_nknow}
             </button>
 
           </div>
         </div>
-
-        <button style={{display: 'none'}} className="hvr-float-shadow" onClick={this.redirect.bind(this)}>
-          Sleep Learn Now...
-        </button>
       </div>
 
     );
